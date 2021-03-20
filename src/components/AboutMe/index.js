85387f3cba20679e59e6bc48a93a10a605278743
Nick May <nickmay9@outlook.com>
2021-03-20 16:46:52 -0400

    added slideshow to about section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './about.css';
+
 import cfImage from '../../assets/images/0E2D6474-F560-4EFD-899A-31E4F9C6A190.jpeg';
 import surfImage from '../../assets/images/58EB85D3-3812-4221-BE02-77E0C26656BA_1_105_c.jpeg';
 import hikeImage from '../../assets/images/EF33746B-78CC-403E-9EE4-80961DE77034_1_105_c.jpeg';
 
 function About() {
+    const slideImages = [
+        hikeImage,
+        surfImage,
+        cfImage
+    ];
 
     return (
-        <section className="aboutSection">
-            <div className="imageContainer">
-                <div className="topLeftImage">
-                    <img src={hikeImage} alt="Hiking" />
-                </div>
-                <div className="topRightImage">
-                    <img src={surfImage} alt="Surfing" />
-                </div>
-                <div className="bottomImage">
-                    <img src={cfImage} alt="Crossfit" />
-                </div>
+        <section>
+            <div className="title">
+                <h1>About Me</h1>
             </div>
             <p className="aboutMe">
                 I'm a graduate of the University of North Florida where I earned my Bachelor's in Electrical
@@ -29,6 +30,11 @@ function About() {
                 fell in love with it and now travel the world to surf. I also found crossfit and I am now getting into doing
                 triathlon's.
             </p>
+            <div className="slide-container">
+                <Slide>
+                    {slideImages.map((each, index) => <img key={index} style={{width: "100%"}} src={each} />)}
+                </Slide>
+            </div>
         </section>
     );
 }
