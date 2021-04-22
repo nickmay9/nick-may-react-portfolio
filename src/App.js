c9c783c8a61760e899063e6b3f945fdb41326dfa
Nick May <37218737+nickmay9@users.noreply.github.com>
2021-04-22 12:24:21 -0400

    Revert "merge"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
